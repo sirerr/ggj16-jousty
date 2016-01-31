c34f63ad9d5f95705e12f26b51a05142f0a79360
sirerr <sirerr@gmail.com>
2016-01-31 00:41:51 -0500

    getting up to date

---
+++ unity/ggj16-jousty/Assets/Scripts/birdbehavoir.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdbehavoir : MonoBehaviour {
+
+	public float birdspeed =0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate(Vector3.back * birdspeed * Time.deltaTime);
+	
+	}
+}
